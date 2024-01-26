de3f89a2484af524da6f804ea5107c2bdd6f847c
stevan002 <stevanstankovic603@gmail.com>
2024-01-26 17:09:07 +0100

    Add configuration and UI for app

---
+++ GroupChat/src/main/java/com/example/GroupChat/chat/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.example.GroupChat.chat;
+
+import lombok.*;
+
+import java.awt.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ GroupChat/src/main/java/com/example/GroupChat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.example.GroupChat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
