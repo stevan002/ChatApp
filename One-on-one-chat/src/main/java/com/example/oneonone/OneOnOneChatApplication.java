4053ce74c4a47ea899d818ac3b63eafd3129df47
stevan002 <stevanstankovic603@gmail.com>
2024-01-29 18:49:52 +0100

    Edit diagram and create project for one-on-one chat app

---
+++ One-on-one-chat/src/main/java/com/example/oneonone/OneOnOneChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.oneonone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneOnOneChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneOnOneChatApplication.class, args);
+	}
+
+}
---
+++ One-on-one-chat/src/test/java/com/example/oneonone/OneOnOneChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oneonone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneOnOneChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
